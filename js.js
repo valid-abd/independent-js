2b6eba3e5bc794e51be876731c35c1739d38c46b
Valid <89277114142@mail.ru>
2020-01-18 21:32:47 +0400

    выполнен второй урок простого задания

---
+++ js.js
@@ -0,0 +1,19 @@
+let myVar;
+                            // Простые типы 
+myVar = 10;             // число 
+myVar = "Hello world";  // строка 
+myVar = true ;          // булевое значение
+myVar = null ;          // нулевое значение
+myVar = undefined ;     // не присвоенное значение
+myVar = Symbol() ;
+
+                             // Обьекты 
+myVar = {} ;
+
+myVar = [] ;
+let regExp = /w+/g;
+let func = function(){};
+let error = Error('Error message');
+let date = new Date();
+
+
--- lesson 02/js/script.js
+++ lesson 02/js/script.js
@@ -1,13 +1,19 @@
-let money = 12000 ,
-    incom = "фриланс" ,
-    addExpenses = "коммуналка учеба дорога",
-    deposit = true,
-    mission = 45000,
-    period = 12 ;
+let money = 12000 ,             // доход за месяц
+    income = "фриланс" ,    // дополнительный доход 
+    addExpenses = "коммуналка учеба дорога",    // доп. расходы
+    deposit = true,         // 
+    mission = 45000,        // цель суммы
+    period = 12 ,           // периуд 
+    budgetDay = money/30    // Доход за месяц
+
 
     console.log(money);
-    console.log(incom);
+    console.log(income);
     console.log(deposit);
-    console.log(incom.length);
+    console.log(income.length);
+    console.log(budgetDay);
+
     console.log(" Период " + period+ " месяцев");
     console.log(" Цель заработать " + mission+ " руб");
+    console.log(addExpenses.toLowerCase().split(' '));
+    
